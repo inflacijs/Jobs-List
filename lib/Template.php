3cc7f78fcb87d74c757c7b395154d353122fb896
Martins Locans <inflacijs@gmail.com>
2020-05-05 13:33:02 +0300

    Template class

---
+++ lib/Template.php
@@ -0,0 +1,31 @@
+<?php 
+    class Template{
+       //Path To Template 
+       protected $template;
+       //Vars passed in
+       protected $vars = array();
+
+       //constructor
+       public function __construct($template){
+            $this->template = $template;
+       }
+
+       public function __get($key){
+           return $this->vars[$key];
+       }
+
+       public function __set($key, $value){
+            $this->$vars[$key] = $value;
+       }
+
+       public function __toString(){
+           extract($this->vars); // This allows inside a template  call $name, instead of $template->name
+           chdir(dirname($this->template)); //Define a path name
+           ob_start(); // Turn on output buffering
+
+           include basename($this->template); // Cut off all path, leaves only file name 
+
+           return ab_get_clean(); // Get current buffer contents and delete current output buffer
+
+       }
+    }
