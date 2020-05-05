a9af7f298f1c4012b71b99a7183299baed298fdc
Martins Locans <inflacijs@gmail.com>
2020-05-05 19:34:16 +0300

    Job class added

--- config/init.php
+++ config/init.php
@@ -1,7 +1,7 @@
 <?php
 //Config file
 require_once 'config.php';
-// Autoloader
+// Autoloader. Helps to call any class from lib folder, without requiring or including
 
 function __autoload($class_name){   
     require_once 'lib/' . $class_name . '.php';
